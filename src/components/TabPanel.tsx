9fef3de8edb2917affae7344731f03caf21de87a
sumikyu <lucia.angulopicazo@gmail.com>
2024-05-20 18:12:16 +0200

    Created TabPanel component

---
+++ src/components/TabPanel.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { Tab } from "./Tab";
+
+interface TabPanel {
+  title: string;
+  tabs: string[];
+  children: React.ReactNode[];
+  content?: React.ReactNode;
+}
+
+const TabPanel = ({
+  tabs,
+  children,
+}: {
+  tabs: TabPanel[];
+  children: TabPanel[];
+}) => {
+  const [selectedTab, setSelectedTab] = useState(0);
+  return (
+    <div>
+      <div role="tablist" aria-label="Tabs">
+        {tabs.map((tab, index) => (
+          <Tab
+            key={index}
+            isSelected={selectedTab === index}
+            onClick={() => setSelectedTab(index)}
+            children={tab.title}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
